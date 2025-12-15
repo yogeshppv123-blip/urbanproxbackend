5b1480be5b32eb0c010cc22b18db158d6cb5b698
yogeshppv123-blip <yogeshppv123@gmail.com>
2025-12-15 10:50:50 +0530

    fix: Deploy latest backend fixes to Render

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').join(__dirname, '..', '..', '.env') });
+require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') });
 const http = require('http');
 const app = require('./app');
 const connectDB = require('./config/db');
